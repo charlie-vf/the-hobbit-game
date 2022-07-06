6d32a6c79322e517a6a2ef630987827fa7e7fdd5
Charlie <charlottevfarmer@gmail.com>
2022-07-06 10:58:36 +0000

    Add about_game function to tell player about the game

--- run.py
+++ run.py
@@ -2,6 +2,15 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def about_game():
+    print(
+        "Welcome to the Hobbit adventure game!\n"
+        "The Dwarves of Erebor are in need of someone to join their quest.\n"
+        "They seek to reclaim their home from the terrible dragon, Smaug.\n"
+        "This adventure is not for the weak of heart.\n"
+    )
+
+
 def choose_name():
     """
     Asks player to choose a character name
@@ -10,4 +19,9 @@ def choose_name():
 
     print(f"Welcome to the company, {name_input}!")
 
+
+def main():
+    about_game()
     choose_name()
+
+main()
