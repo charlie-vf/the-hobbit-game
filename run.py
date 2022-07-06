89c5429cf6d5b759f3098e44879695324624dbb0
Charlie <charlottevfarmer@gmail.com>
2022-07-06 10:49:41 +0000

    Add choose_name function

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def choose_name():
+    """
+    Asks player to choose a character name
+    """
+    name_input = input("Please choose a name for your character!")
+
+    print(f"Welcome to the company, {name_input}!")
+
+    choose_name()
